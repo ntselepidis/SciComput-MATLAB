d541bf7f455e42d36ce18b3b9eb43bc0006dbba6
ntselepidis <ntselepidis@student.ethz.ch>
2020-04-30 01:31:57 +0200

    Added stiffness matrix computation functions.

--- fem/tfem_assemble.m
+++ fem/tfem_assemble.m
@@ -9,9 +9,6 @@ indj = zeros(7*n,1);
 vval = zeros(7*n,1);
 cnt = 1;
 b = zeros(n,1);
-W = zeros(3);
-M = zeros(3);
-f = zeros(3,1);
 for e = 1:ne
     x1 = coo(con(e,1),1);
     x2 = coo(con(e,2),1);
@@ -19,30 +16,7 @@ for e = 1:ne
     y1 = coo(con(e,1),2);
     y2 = coo(con(e,2),2);
     y4 = coo(con(e,3),2);
-    y41 = y4 - y1;
-    y12 = y1 - y2;
-    x14 = x1 - x4;
-    x21 = x2 - x1;
-    J = 0.25*((x2 - x1)*(y4 - y1) - (x4 - x1)*(y2 - y1));
-    W(1,1) = (x14^2 + 2*x14*x21 + x21^2 + y12^2 + 2*y12*y41 + y41^2)/(8*J);
-    W(2,2) = (x14^2 + y41^2)/(8*J);
-    W(3,3) = (x21^2 + y12^2)/(8*J);
-    W(1,2) = -(x14^2 + x21*x14 + y41^2 + y12*y41)/(8*J);
-    W(1,3) = -(x21^2 + x14*x21 + y12^2 + y41*y12)/(8*J);
-    W(2,3) = (x14*x21 + y12*y41)/(8*J);
-    W(2,1) = W(1,2); W(3,1) = W(1,3);
-    W(3,2) = W(2,3);
-    M(1,1) = J/3;
-    M(2,2) = J/3;
-    M(3,3) = J/3;
-    M(1,2) = J/6;
-    M(1,3) = J/6;
-    M(2,3) = J/6;
-    M(2,1) = M(1,2); M(3,1) = M(1,3);
-    M(3,2) = M(2,3);
-    f(1) = (2*J)/3;
-    f(2) = (2*J)/3;
-    f(3) = (2*J)/3;
+    [W, M, f] = tfem_stiffness(x1, y1, x2, y2, x4, y4);
     for i = 1:3
         for j = 1:3
             indi(cnt) = con(e,i);
@@ -54,5 +28,5 @@ for e = 1:ne
         b(con(e,i)) = b(con(e,i)) + f(i);
     end
 end
-A = sparse(indi,indj,vval,n,n);
+A = sparse(indi, indj, vval, n, n);
 end
