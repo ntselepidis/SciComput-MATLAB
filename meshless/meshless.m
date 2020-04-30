8f3b471bce7a9e8c7d6d98262951b7fb208724c2
ntselepidis <ntselepidis@student.ethz.ch>
2020-04-30 02:39:36 +0200

    Code cleanup.

--- meshless/meshless.m
+++ meshless/meshless.m
@@ -1,55 +1,55 @@
-% Lu=0 (2D) on a square domain [0,1]x[0,1]
+% Lu = 0 (2D) on a square domain [0,1]x[0,1]
 % neumann on south boundary
 % dirichlet on the other boundaries
-clear;clc;close all;
-u=@(x,y) exp(x).*cos(y);
-phi=@(r,c) sqrt(r.^2+c^2);
-% r=@(x,xj,y,yj) sqrt((x-xj).^2+(y-yj).^2);
-% dux=@(x,xj,y,yj,c) (x-xj)./sqrt(r(x,xj,y,yj).^2+c^2);
-% du2x=@(x,xj,y,yj,c) ((y-yj).^2+c^2)./((r(x,xj,y,yj,c)+c^2)^(3/2));
-c=1;
-a=0;
-b=1;
-nx=25;
-h=1/(nx-1);
-xo=[a:h:b a:h:b repmat(a,1,nx-2) repmat(b,1,nx-2)]';
-yo=[repmat(a,1,nx) repmat(b,1,nx) a+h:h:b-h a+h:h:b-h]';
-[xi,yi]=meshgrid((a+h):h:(b-h)); % inner points
-xi=xi(:);
-yi=yi(:);
-figure, plot(xo,yo,'r*',xi,yi,'*')
-x=[xo;xi];
-y=[yo;yi];
-n=length(x);
-no=length(xo);
-r=zeros(n);
-dux=zeros(n);
-duy=zeros(n);
-du2x=zeros(n);
-du2y=zeros(n);
-for i=1:n
-    j=setdiff(1:n,i);
-    r(i,j)=sqrt( (x(i)-x(j)).^2 + (y(i)-y(j)).^2 )';
+clear; clc; close all;
+u = @(x,y) exp(x).*cos(y);
+phi = @(r,c) sqrt(r.^2+c^2);
+% r = @(x,xj,y,yj) sqrt((x-xj).^2+(y-yj).^2);
+% dux = @(x,xj,y,yj,c) (x-xj) ./ sqrt(r(x,xj,y,yj).^2+c^2);
+% du2x = @(x,xj,y,yj,c) ((y-yj).^2+c^2) ./ ((r(x,xj,y,yj,c)+c^2)^(3/2));
+c = 1;
+a = 0;
+b = 1;
+nx = 25;
+h = 1 / (nx-1);
+xo = [a:h:b a:h:b repmat(a, 1, nx-2) repmat(b, 1, nx-2)]';
+yo = [repmat(a, 1, nx) repmat(b, 1, nx) a+h:h:b-h a+h:h:b-h]';
+[xi, yi] = meshgrid((a+h):h:(b-h)); % inner points
+xi = xi(:);
+yi = yi(:);
+figure, plot(xo, yo, 'r*', xi, yi, '*')
+x = [xo; xi];
+y = [yo; yi];
+n = length(x);
+no = length(xo);
+r = zeros(n);
+dux = zeros(n);
+duy = zeros(n);
+du2x = zeros(n);
+du2y = zeros(n);
+for i = 1:n
+    j = setdiff(1:n, i);
+    r(i,j) = sqrt( (x(i)-x(j)).^2 + (y(i)-y(j)).^2 )';
 end
-j=1:n;
-for i=1:n
-    dux(i,j)=(x(i)-x(j))./(sqrt(r(i,j).^2+c^2))';
-    du2x(i,j)=((y(i)-y(j)).^2+c^2)./((r(i,j).^2+c^2).^(3/2))';  
-    duy(i,j)=(y(i)-y(j))./(sqrt(r(i,j).^2+c^2))';
-    du2y(i,j)=((x(i)-x(j)).^2+c^2)./((r(i,j).^2+c^2).^(3/2))';
+j = 1:n;
+for i = 1:n
+    dux(i,j) = (x(i)-x(j)) ./ (sqrt(r(i,j).^2+c^2))';
+    du2x(i,j) = ((y(i)-y(j)).^2+c^2) ./ ((r(i,j).^2+c^2).^(3/2))';  
+    duy(i,j) = (y(i)-y(j)) ./ (sqrt(r(i,j).^2+c^2))';
+    du2y(i,j) = ((x(i)-x(j)).^2+c^2) ./ ((r(i,j).^2+c^2).^(3/2))';
 end
-A=du2x+du2y;
-% A(1:no,1:no)=eye(no);
-% A(1:no,(no+1):n)=zeros(no,length((no+1):n));
-A(1:no,1:n)=phi(r(1:no,1:n),c);
-b=zeros(n,1);
-b(1:no)=u(x(1:no),y(1:no));%exp(x(1:no)).*cos(y(1:no));
-b(1:nx)=zeros(nx,1);
-A(1:nx,:)=duy(1:nx,:);
+A = du2x+du2y;
+% A(1:no,1:no) = eye(no);
+% A(1:no,(no+1):n) = zeros(no,length((no+1):n));
+A(1:no,1:n) = phi(r(1:no,1:n),c);
+b = zeros(n, 1);
+b(1:no) = u(x(1:no),y(1:no));%exp(x(1:no)).*cos(y(1:no));
+b(1:nx) = zeros(nx, 1);
+A(1:nx,:) = duy(1:nx,:);
 figure, imagesc(A)
-xx=A\b;
-exact=u(x,y);
-xx=phi(r(1:n,1:n),c)*xx;
-error=norm(xx-exact)/norm(exact)
-figure, plot3(x,y,exact,'*')
-figure, plot3(x,y,xx,'*')
+xx = A \ b;
+exact = u(x,y);
+xx = phi(r(1:n,1:n), c)*xx;
+error = norm(xx-exact) / norm(exact)
+figure, plot3(x, y, exact,'*')
+figure, plot3(x, y, xx, '*')
--- meshless/meshless2.m
+++ meshless/meshless2.m
@@ -1,62 +1,62 @@
-% Lu=0 (2D) on a square domain [0,1]x[0,1]
+% Lu = 0 (2D) on a square domain [0,1]x[0,1]
 % neumann on south boundary
 % dirichlet on the other boundaries
-clear;clc;close all;
+clear; clc; close all;
 rng(0); % used for reproducibility
-u=@(x,y) exp(x).*cos(y);
-phi=@(r,c) sqrt(r.^2+c^2);
-c=1; % shape parameter
-a=0;
-b=1;
-nx=25;
-h=1/(nx-1);
-xo=[a:h:b a:h:b repmat(a,1,nx-2) repmat(b,1,nx-2)]';
-yo=[repmat(a,1,nx) repmat(b,1,nx) a+h:h:b-h a+h:h:b-h]';
-% [xi,yi]=meshgrid((a+h):h:(b-h)); % inner points
-% xi=xi(:);
-% yi=yi(:);
-xiyi=rand((nx-4)*nx,2);
-xiyi(xiyi(:,1)==a,:)=[];
-xiyi(xiyi(:,1)==b,:)=[];
-xiyi(xiyi(:,2)==a,:)=[];
-xiyi(xiyi(:,2)==b,:)=[];
-xi=xiyi(:,1); yi=xiyi(:,2);
-figure, plot(xo,yo,'r*',xi,yi,'*'), grid
-x=[xo;xi];
-y=[yo;yi];
-n=length(x);
-no=length(xo);
-r=zeros(n);
-dphix=zeros(n);
-dphiy=zeros(n);
-dphi2x=zeros(n);
-dphi2y=zeros(n);
-for i=1:n
-    j=setdiff(1:n,i);
-    r(i,j)=sqrt( (x(i)-x(j)).^2 + (y(i)-y(j)).^2 )';
+u = @(x,y) exp(x).*cos(y);
+phi = @(r,c) sqrt(r.^2+c^2);
+c = 1; % shape parameter
+a = 0;
+b = 1;
+nx = 25;
+h = 1 / (nx-1);
+xo = [a:h:b a:h:b repmat(a, 1, nx-2) repmat(b, 1, nx-2)]';
+yo = [repmat(a, 1, nx) repmat(b, 1, nx) a+h:h:b-h a+h:h:b-h]';
+% [xi, yi] = meshgrid((a+h):h:(b-h)); % inner points
+% xi = xi(:);
+% yi = yi(:);
+xiyi = rand((nx-4)*nx, 2);
+xiyi(xiyi(:,1)==a,:) = [];
+xiyi(xiyi(:,1)==b,:) = [];
+xiyi(xiyi(:,2)==a,:) = [];
+xiyi(xiyi(:,2)==b,:) = [];
+xi = xiyi(:,1); yi = xiyi(:,2);
+figure, plot(xo, yo, 'r*', xi, yi, '*'), grid
+x = [xo; xi];
+y = [yo; yi];
+n = length(x);
+no = length(xo);
+r = zeros(n);
+dphix = zeros(n);
+dphiy = zeros(n);
+dphi2x = zeros(n);
+dphi2y = zeros(n);
+for i = 1:n
+    j = setdiff(1:n, i);
+    r(i,j) = sqrt( (x(i)-x(j)).^2 + (y(i)-y(j)).^2 )';
 end
-j=1:n;
-for i=1:n
-    dphix(i,j)=(x(i)-x(j))./(sqrt(r(i,j).^2+c^2))';
-    dphiy(i,j)=(y(i)-y(j))./(sqrt(r(i,j).^2+c^2))';
-    dphi2x(i,j)=((y(i)-y(j)).^2+c^2)./((r(i,j).^2+c^2).^(3/2))';  
-    dphi2y(i,j)=((x(i)-x(j)).^2+c^2)./((r(i,j).^2+c^2).^(3/2))';
+j = 1:n;
+for i = 1:n
+    dphix(i,j) = (x(i)-x(j)) ./ (sqrt(r(i,j).^2+c^2))';
+    dphiy(i,j) = (y(i)-y(j)) ./ (sqrt(r(i,j).^2+c^2))';
+    dphi2x(i,j) = ((y(i)-y(j)).^2+c^2) ./ ((r(i,j).^2+c^2).^(3/2))';  
+    dphi2y(i,j) = ((x(i)-x(j)).^2+c^2) ./ ((r(i,j).^2+c^2).^(3/2))';
 end
-A=dphi2x+dphi2y;
-b=zeros(n,1);
+A = dphi2x+dphi2y;
+b = zeros(n, 1);
 % Dirichlet Boundaries
-A(1:no,:)=phi(r(1:no,:),c);
-b(1:no)=u(x(1:no),y(1:no));
+A(1:no,:) = phi(r(1:no,:),c);
+b(1:no) = u(x(1:no),y(1:no));
 % Neumann Boundaries (overwrite south boundary)
-A(1:nx,:)=dphiy(1:nx,:);
-b(1:nx)=zeros(nx,1);
+A(1:nx,:) = dphiy(1:nx,:);
+b(1:nx) = zeros(nx, 1);
 % Solve
-w=A\b;
-xx=phi(r,c)*w;
-exact=u(x,y);
-error=norm(xx-exact)/norm(exact)
+w = A \ b;
+xx = phi(r,c)*w;
+exact = u(x,y);
+error = norm(xx-exact) / norm(exact)
 % figure, imagesc(A);
-figure, plot3(x,y,exact,'*'), grid, title('Exact solution');
-figure, plot3(x,y,xx,'*'), grid, title('Meshless solution');
+figure, plot3(x, y, exact, '*'), grid, title('Exact solution');
+figure, plot3(x, y, xx, '*'), grid, title('Meshless solution');
 % figure, plot3(x(1:no),y(1:no),xx(1:no),'r*', ...
 %               x(no+1:end),y(no+1:end),xx(no+1:end),'b*')
