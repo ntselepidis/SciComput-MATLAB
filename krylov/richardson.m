8113214877d8f63fa144c36e9a77d97646ef240e
Nikolaos Tselepidis <ntselepidis@student.ethz.ch>
2022-05-19 00:49:00 +0200

    Modified richardson iteration.

--- krylov/richardson.m
+++ krylov/richardson.m
@@ -1,33 +1,34 @@
 function [x, val_record] = richardson(A, b, tol, maxit, M, dummy, x0)
     flag = 0;
-    % initial estimate
+    normb = norm(b);
+    tolb = tol*normb;
     if nargin < 7
         x = zeros(length(b), 1);
     else
         x = x0;
     end
-    % richardson iteration
-    normb = norm(b);
-    val0 = 0.5*( x'*A(x) ) - x'*b;
-    val1 = inf;
+    val1 = 0.5*( x'*A(x) ) - x'*b;
     val_record = zeros(maxit+1, 1);
-    val_record(1) = val0;
+    val_record(1) = val1;
+    r = b - A(x);
+    p = M(r);
     for i = 1 : maxit
-        r = b - A(x);
-        %relres = norm(r) / normb;
-        %if (relres <= tol)
-        %if (abs(val1 - val0)/abs(val0) <= tol)
-        %    disp(['richardson iteration converged in ' num2str(i) ' iterations with relative residual ' num2str(relres) '.'])
-        %    flag = 1;
-        %    break;
-        %end
-        %val0 = val1;
-        rhat = M(r);
-        %omega = (rhat'*rhat) / (rhat'*(A(M(rhat))));
-        omega = (r'*rhat) / (rhat'*A(rhat));
-        x = x + omega * rhat;
+        Ap = A(p);
+        %alpha = (p'*p) / (p'*(A(M(p))));
+        alpha = (r'*p) / (p'*Ap);
+        x = x + alpha*p;
+        r = r - alpha*Ap;
+        val0 = val1;
         val1 = 0.5*( x'*A(x) ) - x'*b;
         val_record(i+1) = val1;
+        normr = norm(r);
+        %if (normr <= tolb)
+        %%if (abs(val1 - val0)/abs(val0) <= tol)
+        %    flag = 1;
+        %    disp(['richardson iteration converged in ' num2str(i) ' iterations with relative residual ' num2str(normr/normb) '.'])
+        %    break;
+        %end
+        p = M(r);
     end
     %if flag == 0
     %    disp(['richardson iteration failed to converge within ' num2str(maxit) ' iterations.'])
