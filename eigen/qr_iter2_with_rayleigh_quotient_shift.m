e49fb40c2730f797e91cd7e1114765a067ddccf4
Nikolaos Tselepidis <nikotselep@gmail.com>
2024-10-25 15:22:17 +0200

    Fixed iters reported in QR iteration with single shift.

--- eigen/qr_iter2_with_rayleigh_quotient_shift.m
+++ eigen/qr_iter2_with_rayleigh_quotient_shift.m
@@ -2,7 +2,7 @@ function [Qp,T]=qr_iter2_with_rayleigh_quotient_shift(H,tol,maxit)
 [n,~]=size(H);
 Qp=eye(n,n); % Qp = Q(1)*Q(2)*...*Q(maxit).
 k=0;
-iters=-1;
+iters=0;
 for m=n:-1:2
     while (1)
         k = k + 1;
@@ -11,12 +11,12 @@ for m=n:-1:2
         H = H + sigma_k*eye(n,n);
         Qp = Qp*Q;
         if ( norm(H(m,m-1))<tol )
-            iters=k;
+            iters=iters+k;
             break;
         end
     end
 end
-if (iters~=-1)
+if (iters~=0)
     disp(['QR algorithm converged in ' num2str(iters) ' iterations.'])
 else
     disp(['QR algorithm did not converge within ' num2str(maxit) ...
