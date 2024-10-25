48d2bcd57bd86185692adb9c90b4f9762d759d55
Nikolaos Tselepidis <nikotselep@gmail.com>
2024-10-25 11:44:38 +0200

    Implemented hessenberg reduction.

---
+++ eigen/hess2.m
@@ -0,0 +1,24 @@
+function H = hess2(A)
+    [n, m] = size(A);
+    if (n ~= m)
+        error('Matrix must be square');
+    end
+    H = A;
+    for k = 1:n-2
+        x = H(k+1:n, k);
+        e1 = eye(length(x), 1);
+
+        if ( x(1) >= 0 )
+            sn = 1;
+        else
+            sn = -1;
+        end
+        %sn = exp(j*angle(x(1)));
+
+        u = sn*norm(x)*e1+x;
+        u = u / norm(u);
+
+        H(k+1:n, k:n) = H(k+1:n, k:n) - (2*u)*(u'*H(k+1:n, k:n));
+        H(1:n, k+1:n) = H(1:n, k+1:n) - (H(1:n, k+1:n)*u)*(2*u)';
+    end
+end
