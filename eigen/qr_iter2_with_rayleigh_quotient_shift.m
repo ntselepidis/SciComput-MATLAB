181ae716fb8edd73dc4a2804bb384bff3bc9ccc0
Nikolaos Tselepidis <nikotselep@gmail.com>
2024-10-30 21:15:13 +0100

    Fixed iteration counting.

--- eigen/qr_iter2_with_rayleigh_quotient_shift.m
+++ eigen/qr_iter2_with_rayleigh_quotient_shift.m
@@ -1,19 +1,16 @@
 function [Qp,T]=qr_iter2_with_rayleigh_quotient_shift(H,tol,maxit)
 [n,~]=size(H);
 Qp=eye(n,n); % Qp = Q(1)*Q(2)*...*Q(maxit).
-k=0;
 iters=0;
 maxit_inner = floor(maxit / n);
-%maxit_inner = n;
 for m=n:-1:2
     for iter_inner = 1 : maxit_inner
-        k = k + 1;
         sigma_k = H(m,m);
         [~,H(1:m,1:m)] = hess_qr(H(1:m,1:m) - sigma_k*eye(m,m)); % H = QR, Hbar = RQ, H = Hbar, in-place
         H(1:m,1:m) = H(1:m,1:m) + sigma_k*eye(m,m);
         %Qp = Qp*Q;
         if ( abs(H(m,m-1)/H(m,m))<tol )
-            iters=iters+k;
+            iters=iters+iter_inner;
             break;
         end
     end
